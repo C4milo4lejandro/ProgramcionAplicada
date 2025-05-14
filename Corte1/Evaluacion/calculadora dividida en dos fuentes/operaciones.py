0697fbf49aaee180b8be3465fc37fec2164f7646
C4milo4lejandro <caacameloc@udistrital.edu.co>
2025-05-14 12:38:20 -0500

    Create operaciones.py

---
+++ Corte1/Evaluacion/calculadora dividida en dos fuentes/operaciones.py
@@ -0,0 +1,18 @@
+# operaciones.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No se puede dividir entre cero"
+    return a / b
+
+def potencia(a, b):
+    return a ** b
